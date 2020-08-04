8d88a1fb3c167862186aa478179657b45dc67c70
mohammed-allou <60431576+mohammed-allou@users.noreply.github.com>
2020-08-04 12:11:30 +0100

    apres modification

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,49 +1,49 @@
 <template>
   <v-app>
-    <v-data-table
-     
-      :id="id"
-      :nom="nom"
-      :prix="prix"
-      :Libellé="Libellé"
-      :description="description"
-    ></v-data-table>
+    <v-card class="mx-auto" max-width="344" v-for="produit in produits" :key="produit.id">
+      <v-card-text >
+        <div>Word of the Day</div>
+        <p class="display-1 text--primary">{{produit.nom}}</p>
+        <p>{{produit.id}}</p>
+        <div class="text--primary">
+          {{produit.prix}}
+          
+        </div>
+        <p>{{produit.Libelle}}</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="deep-purple accent-4">Description</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-app>
 </template>
+
 <script lang="ts">
 import Vue from "vue";
-import axios from "axios"
+import axios from "axios";
 
-
-const baseURL =
-  "https://cddf91f5-abd8-4055-9f24-5e852da192c6.mock.pstmn.io/produits";
 export default Vue.extend({
   name: "list",
   data() {
     return {
       produits: [],
-      headers: [
-        { text: " numero de produit", value: "id" },
-        { text: " nom de produit", value: "nom" },
-        { text: " prix de produit", value: "prix" },
-        { text: " libellé", value: "Libellé" },
-        { text: " description de produit", value: "description" },
-      ],
+   
     };
   },
-  methods: {
-    ajoutAPI() {
-      axios.get(baseURL).then((res) => {
-        this.id = res.data.id;
-        this.nom = res.data.nom;
-        this.prix = res.data.prix;
-        this.Libellé = res.data.Libellé;
-        this.description = res.data.description;
-      });
-    },
+  mounted() {
+    
+      axios
+        .get(
+          "https://cddf91f5-abd8-4055-9f24-5e852da192c6.mock.pstmn.io/produits"
+        )
+        .then((response) => {
+         
+          console.log(response)
+          this.produits = response.data;
+          
+        });
+
   },
-  mounted(){
-      this.ajoutAPI();
-  }
+   
 });
 </script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  axios,
   render: h => h(App)
 }).$mount('#app')
+ const auth = axios.create({
+   baseURL: "https://cddf91f5-abd8-4055-9f24-5e852da192c6.mock.pstmn.io/produits"
+ })
+ auth.defaults.headers.common['auth']='produits'
+ export default auth
