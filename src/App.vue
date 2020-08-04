9a388736f5f74dfac531e3080d9edf0351f9dce6
mohammed-allou <60431576+mohammed-allou@users.noreply.github.com>
2020-08-04 10:05:19 +0100

    mise en page initiale

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,34 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app color="primary" dark >
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <h2 class="text-center">Listes des produits</h2>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
+      <list/>
+      
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
+import list from "./components/list"
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    list,
+},
+// mounted(){
+//   produits.listes().then(Response =>{
+//     console.log(Response)
+//   })
+// }
+}
 </script>
---
+++ src/components/list.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-data-table
+     
+      :id="id"
+      :nom="nom"
+      :prix="prix"
+      :Libellé="Libellé"
+      :description="description"
+    ></v-data-table>
+  </v-app>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import axios from "axios"
+
+
+const baseURL =
+  "https://cddf91f5-abd8-4055-9f24-5e852da192c6.mock.pstmn.io/produits";
+export default Vue.extend({
+  name: "list",
+  data() {
+    return {
+      produits: [],
+      headers: [
+        { text: " numero de produit", value: "id" },
+        { text: " nom de produit", value: "nom" },
+        { text: " prix de produit", value: "prix" },
+        { text: " libellé", value: "Libellé" },
+        { text: " description de produit", value: "description" },
+      ],
+    };
+  },
+  methods: {
+    ajoutAPI() {
+      axios.get(baseURL).then((res) => {
+        this.id = res.data.id;
+        this.nom = res.data.nom;
+        this.prix = res.data.prix;
+        this.Libellé = res.data.Libellé;
+        this.description = res.data.description;
+      });
+    },
+  },
+  mounted(){
+      this.ajoutAPI();
+  }
+});
+</script>
